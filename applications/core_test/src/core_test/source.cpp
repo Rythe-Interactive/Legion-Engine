09ce7e55d7d4cdf860b981821336bce0a64bc61a
Glyn Leine <g.m.leine@gmail.com>
2024-12-10 17:47:47 +0100

    small rework of entry point and some error handling things

--- applications/core_test/src/core_test/source.cpp
+++ applications/core_test/src/core_test/source.cpp
@@ -8,8 +8,38 @@
 
 #include <core/core.hpp>
 
-void RYTHE_CCONV reportModules(rythe::core::Program* program)
+enum struct test_error : rsl::errc
 {
+    no_error = 0,
+    hi,
+    no,
+    fuck,
+    you
+};
+
+void assert_handler(
+	std::string_view expression, std::string_view file, rsl::size_type line, std::string_view message, bool soft
+)
+{
+	rsl::log::undecoratedln(
+		soft ? rsl::log::severity::warn : rsl::log::severity::fatal,
+		"Assertion failed:\t{}\nExpected:\t\t{}\nSource:\t\t\t{}, line {}", message, expression, file, line
+	);
+
+	__debugbreak();
+}
+
+rsl::result<void> RYTHE_CCONV init_program(rythe::core::Program& program)
+{
+	rsl::asserts::assert_handler = &assert_handler;
+
+	rsl::result<void> result;
+
 	rsl::log::debug("Initilizing Core-Application");
-	program->addEngineInstance();
+	program.addEngineInstance();
+
+    rsl::append_error(result, test_error::no, "NOPE");
+    rsl::append_warning(result, test_error::hi, "HEEEEYYYYY");
+
+    return result;
 }
--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,12 @@
 -- root workspace, all sub-project should be included
 workspace "rythe"
     location("build/" .. _ACTION)
-    configurations { "Debug", "Development", "Release", "Debug-asan", "Release-profiling" }
+    configurations { "Debug", "Debug-no-inline", "Development", "Release", "Debug-asan", "Release-profiling" }
 
 os.chdir(_MAIN_SCRIPT_DIR)
+
+filter("configurations:Debug-no-inline")
+    defines { "RYTHE_DISABLE_ALWAYS_INLINE" }
 
 local r = require("premake/rythe")
 
