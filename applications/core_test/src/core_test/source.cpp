f250059864fb40d16fe3f8dfdb9c876a810df566
Glyn Leine <g.m.leine@gmail.com>
2024-12-11 23:24:18 +0100

    make some more parts of rsl compile for msvc, type_map, some cleanup around allocator, stl compatible allocator wrapper, add engine and program context, stop assertion of startup errors

--- applications/core_test/src/core_test/source.cpp
+++ applications/core_test/src/core_test/source.cpp
@@ -1,45 +1,20 @@
 #define RYTHE_ENTRY
 
-#include <chrono>
-#include <iostream>
-#include <rsl/logging>
-#include <rsl/math>
-#include <rsl/time>
-
 #include <core/core.hpp>
 
-enum struct test_error : rsl::errc
+using namespace rythe::core;
+
+struct test_struct
 {
-    no_error = 0,
-    hi,
-    no,
-    fuck,
-    you
+	int i = 14;
 };
 
-void assert_handler(
-	std::string_view expression, std::string_view file, rsl::size_type line, std::string_view message, bool soft
-)
+rsl::result<void> RYTHE_CCONV init_program(Program& program)
 {
-	rsl::log::undecoratedln(
-		soft ? rsl::log::severity::warn : rsl::log::severity::fatal,
-		"Assertion failed:\t{}\nExpected:\t\t{}\nSource:\t\t\t{}, line {}", message, expression, file, line
-	);
+	auto& engine = program.addEngineInstance();
+	engine.get_context().emplace<test_struct>().i = 15;
 
-	__debugbreak();
-}
+    rsl::log::debug("{}", engine.get_context().get<test_struct>().i);
 
-rsl::result<void> RYTHE_CCONV init_program(rythe::core::Program& program)
-{
-	rsl::asserts::assert_handler = &assert_handler;
-
-	rsl::result<void> result;
-
-	rsl::log::debug("Initilizing Core-Application");
-	program.addEngineInstance();
-
-    rsl::append_error(result, test_error::no, "NOPE");
-    rsl::append_warning(result, test_error::hi, "HEEEEYYYYY");
-
-    return result;
+    return rsl::error; // Temporarily to stop the engine from running.
 }
